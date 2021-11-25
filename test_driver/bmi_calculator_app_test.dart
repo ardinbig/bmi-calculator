885f4e1d542a8d078940f4bc92c6e482a9a84a67
Ardin Biganiro <ardinbig@gmail.com>
2021-11-25 18:28:05 +0200

    Taking screenshots

--- test_driver/bmi_calculator_app_test.dart
+++ test_driver/bmi_calculator_app_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
@@ -16,12 +18,27 @@ void main() {
     await driver!.waitFor(find.text("180"));
     await driver!.waitFor(find.text("60"));
     await driver!.waitFor(find.text("19"));
+    await driver!.tackScreenshot("input_page");
   });
 
   test("Calculating BMI with inital value", () async {
     await driver!.tap(find.byType("BottomButton"));
     await driver!.waitFor(find.text("NORMAL"));
     await driver!.waitFor(find.text("18.5"));
-    await driver!.waitFor(find.text("You have a normal body weight. Good job !"));
+    await driver!.waitFor(
+      find.text("You have a normal body weight. Good job !"),
+    );
+    await driver!.tackScreenshot("result_page");
   });
+}
+
+extension on FlutterDriver {
+  Future<void> tackScreenshot(String fileName) async {
+    final filePath = File("screenshots/$fileName.png");
+    if (await filePath.exists()) await filePath.delete(recursive: true);
+    final file = await filePath.create(recursive: true);
+    final png = await screenshot();
+    file.writeAsBytesSync(png);
+    print("Screenshot with the name '$fileName' was taken");
+  }
 }
